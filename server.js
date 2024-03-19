013b0a39f995378cdca6e6a37dc04520dae5dcba
kananmhn <kannanfrlearn@gmail.com>
2024-03-19 12:37:56 +0530

    code update

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('http://localhost:3001/api/update-color', (req, res) => {
-  const { fontColor } = req.body;
-
-  // Update the JSON file
-  fs.writeFileSync('colors.json', JSON.stringify({ fontColor }));
-
-  res.send({ status: 'success' });
-});
-
+app.post('/api/update-color', (req, res) => {
+    const { fontColor } = req.body;
+  
+    // Read content of the file
+    fs.readFile('colors.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading file from disk: ${err}`);
+      } else {
+        // Parse JSON string to JSON object
+        const colors = JSON.parse(data);
+  
+        // Add or update fontColor property
+        colors.fontColor = fontColor;
+  
+        // Write updated color data back to file
+        fs.writeFile('colors.json', JSON.stringify(colors, null, 4), (err) => {
+          if (err) {
+            console.error(`Error writing file to disk: ${err}`);
+          }
+        });
+      }
+    });
+  
+    res.send({ status: 'success' });
+  });
 app.listen(3001, () => console.log('Server listening on port 3001'));
