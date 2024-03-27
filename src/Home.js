8f1c5b9f62867e152014cd87de9b2fe963ca983d
kananmhn <159273393+kananmhn@users.noreply.github.com>
2024-03-27 23:48:46 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import "../App.css";
+
+import FontColor from "../components/FontColor/FontColor";
+import BackgroundColor from "../components/BackgroundColor/BacgroundColor";
+import BtnBgColor from "../components/BtnBgColor/BtnBgColor";
+import BtnTextColor2 from "../components/BtnTextColor/BtnTextColor";
+import FontSize from "../components/FontSize/FontSize";
+import LeftSideComponent from "../components/LeftSideComponent/LeftSideComponent";
+function App() {
+  let [fColor, setFColor] = useState("");
+  let [BGColor, setBGColor] = useState("");
+  let [BtnTextColor1, setBtntextColor] = useState("");
+  let [BtnbgColor, setBtnbgColor] = useState("");
+  let [fontSize, setfontSize] = useState("");
+
+
+  function handleDataFromChildFC(data) {
+    setFColor(data);
+  }
+  function handleDataFromChildBG(data) {
+    setBGColor(data);
+  }
+  function handleDataFromChildBtnBg(data) {
+    setBtnbgColor(data);
+  }
+  function handleDataFromChildBtnTxt(data) {
+    setBtntextColor(data);
+  }
+  function handleDataFromChildFontSize(data) {
+    setfontSize(data);
+  }
+ 
+  fetch('http://localhost:3001/api/color-id')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log('Data from server:', data); // Log the entire data object
+      const color_id = data.color_id;
+      const fColor = data.fontColor;
+      const BGColor = data.backgroundColor;
+      const BtnTextColor1 = data.BtnTextColor;
+      const BtnbgColor = data.BtnBgcolor;
+      const fontSize = data.fontSize;
+      localStorage.setItem('color_id', color_id);
+      localStorage.setItem('fColor', fColor);
+      localStorage.setItem('BGColor', BGColor);
+      localStorage.setItem('BtnTextColor1', BtnTextColor1);
+      localStorage.setItem('BtnbgColor', BtnbgColor);
+      localStorage.setItem('fontSize', fontSize);
+     
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+
+  const applyColor = () => {
+  
+    console.log(fColor, BGColor, BtnTextColor1, BtnbgColor, fontSize);
+
+    if (!fColor) {
+      fColor = localStorage.getItem('fColor'); 
+  }
+  if (!BGColor) {
+      BGColor = localStorage.getItem('BGColor');
+  }
+  if (!BtnTextColor1) {
+      BtnTextColor1 = localStorage.getItem('BtnTextColor1');
+  if (!BtnbgColor) {
+      BtnbgColor =  localStorage.getItem('BtnbgColor');
+  }
+  if (!fontSize) {
+      fontSize =localStorage.getItem('fontSize');
+  }
+    
+    // Send a request to the server to update the JSON file
+    fetch('http://localhost:3001/api/update-color', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fontColor: fColor,
+        backgroundColor: BGColor,
+        BtnTextColor: BtnTextColor1,
+        BtnBgcolor: BtnbgColor,
+        fontSize: fontSize,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+  };
+}
+  return (
+    <div className="App">
+      <div className="max-container">
+        <div className="row content-wrapper">
+          <div className="col-lg-3 left-section">
+            <h3 className="sidebar-title">Select Your Theme</h3>
+            <BackgroundColor sendDataToParent={handleDataFromChildBG} />
+            <FontColor sendDataToParent={handleDataFromChildFC} />
+            <BtnBgColor sendDataToParent={handleDataFromChildBtnBg} />                      
+            <BtnTextColor2 sendDataToParent={handleDataFromChildBtnTxt} />
+            <FontSize sendDataToParent={handleDataFromChildFontSize} />
+            <button className="apply-color" onClick={applyColor}>Apply Color</button>
+          </div>
+          <div className="col-lg-9 right-section" style={{  backgroundColor: BGColor }}>
+             <LeftSideComponent fColor={fColor} BGColor={BGColor} BtnTextColor1={BtnTextColor1} BtnbgColor={BtnbgColor} fontSize={fontSize}/>
+            </div>
+          </div>
+        </div>
+      </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Layout.js
@@ -0,0 +1,76 @@
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import { Modal } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo-1.png';
+
+function Layout() {
+
+    const navigate = useNavigate();
+    const fileInput = useRef();
+
+    const handleImportClick = () => {
+        const modalElement = document.getElementById('uploadModal');
+        const bsModal = new Modal(modalElement);
+        bsModal.show();
+    };
+    const handleCloseModel = () => {
+        let myModalEl = document.getElementById('uploadModal');
+        let myModalE2 = document.querySelector('.modal-backdrop');
+        myModalEl.classList.remove('show');
+        myModalE2.remove();
+        myModalEl.setAttribute('aria-hidden', 'true');
+        myModalEl.setAttribute('style', 'display: none');
+    }
+    const handleFileUpload = () => {
+        const file = fileInput.current.files[0];
+        // Handle the file upload here
+        console.log(file);
+        const modalElement = document.getElementById('uploadModal');
+        const bsModal = new Modal(modalElement);
+        bsModal.hide();
+    };
+  return (
+    <div className="App mt-5">
+            <div className="teamLogo">
+                <img src={logo} className="img-fluid" />
+            </div>
+        <br/>
+        <p>Here you go...if you want to change/import theme from here, you can select one of the following options:</p>
+        <br/>
+        <div className="text-center">
+        <div className="flex-wrapper">
+            <div onClick={() => navigate("/home")} className="select_base_theme">
+                Select Base Theme
+            </div>
+           
+            <div onClick={handleImportClick} className="select_base_theme">
+               import a Theme
+            </div>
+        </div>
+        </div>
+        <div className="modal fade" id="uploadModal" tabIndex="-1" role="dialog" aria-labelledby="uploadModalLabel" aria-hidden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="uploadModalLabel">Upload CSS File</h5>
+                                        <button type="button" onClick={handleCloseModel} className="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <input type="file" ref={fileInput} />
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" onClick={handleCloseModel}  className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" className="btn btn-primary" onClick={handleFileUpload}>Upload</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+      </div>
+   
+  );
+}
+export default Layout;
